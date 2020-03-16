666257414fa037274eec4f49e48691ddc35230ab
Shounak Bhattacharya <shounak.bhattacharya@falconbrick.com>
2020-03-16 22:11:57 +0530

    Changed bundler to rollup, relegating tscc to minified iife file

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
+import tscc from '@tscc/rollup-plugin-tscc';
+import resolve from "@rollup/plugin-node-resolve";
+
+const override = {
+    compilerOptions: {
+        module: "esnext",
+        "declaration": true,
+        "declarationMap": true,
+        "declarationDir": "lib/"
+    }
+};
+
+export default {
+    input: 'src/index.ts',
+    output: [{
+        name: './lib/index.js',
+        format: 'cjs',
+        dir: 'lib'
+    }
+    ],
+    external: [
+        ...Object.keys(pkg.dependencies || {}),
+        ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    plugins: [
+        resolve(),
+        tscc({
+            specFile: "./tscc.spec.json"
+        }),
+        typescript({
+            useTsconfigDeclarationDir: true,
+            tsconfigOverride: override,
+            typescript: require('typescript')
+        })
+    ]
+}
